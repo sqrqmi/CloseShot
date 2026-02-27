fa4094b37dfd5f67c2391bd642e9b8f70f49b367
sqrqmi <xinghong885@gmail.com>
2026-02-28 03:28:13 +0900

    2/28_SquareDraw
    四角形の描画までできるようになりました。

---
+++ sqEngine/Source/DirectX/Texture.cpp
@@ -0,0 +1,47 @@
+#include "framework.h"
+
+#include <locale.h>
+#include <memory>
+
+#include "Texture.h"
+
+//=========================================================
+// 画像ファイルを読み込む
+bool Texture::Load(const std::string& filename)
+{
+	// マルチバイト文字列からワイド文字列へ変換
+	setlocale(LC_CTYPE, "jpn");
+	wchar_t wFilename[256];
+	size_t ret;
+	mbstowcs_s(&ret, wFilename, filename.c_str(), 256);
+
+	// WIC画像を読み込む（WIC : Windows Imaging Component）
+	auto image = std::make_unique<DirectX::ScratchImage>();
+	if ( FAILED( DirectX::LoadFromWICFile(wFilename, DirectX::WIC_FLAGS_NONE, &mInfo, *image) ) )
+	{
+		// 失敗
+		mInfo = {};
+		return false;
+	}
+
+	// ミップマップの作成
+	if ( mInfo.mipLevels == 1 )
+	{
+		auto mipChain = std::make_unique<DirectX::ScratchImage>();
+		if ( SUCCEEDED( DirectX::GenerateMipMaps(image->GetImages(), image->GetImageCount(), image->GetMetadata(), DirectX::TEX_FILTER_DEFAULT, 0, *mipChain) ) )
+		{
+			image = std::move(mipChain);
+		}
+	}
+
+	// リソースとシェーダーリソースビューを作成
+	if ( FAILED( DirectX::CreateShaderResourceView(D3D.mDevice.Get(), image->GetImages(), image->GetImageCount(), mInfo, &mSrv) ) )
+	{
+		// 失敗
+		mInfo = {};
+		return false;
+	}
+
+	// 成功
+	return true;
+}
---
+++ sqEngine/Source/Game/GameSystem.cpp
@@ -0,0 +1,39 @@
+#include "framework.h"
+
+// GameSystemクラスを使えるようにする
+#include "GameSystem.h"
+
+// Direct3Dクラスを使えるようにする
+#include "Source/DirectX/DirectX.h"
+
+//=========================================================
+// ゲームの初期化処理
+void GameSystem::Initialize()
+{
+}
+
+//=========================================================
+// ゲームの更新処理
+void GameSystem::Update()
+{
+	// 画面を青色で塗りつぶす
+	float color[4] = { 0.2f, 0.2f, 1.0f, 1.0f };
+	D3D.mDeviceContext->ClearRenderTargetView(D3D.mBackBufferView.Get(), color);
+
+	// 三角形の描画
+	{
+		std::vector<VertexType2D> squares =
+		{ 
+			{ DirectX::XMFLOAT3(-0.5f, -0.5f, 0.0f) },	// 左下
+			{ DirectX::XMFLOAT3(-0.5f,  0.5f, 0.0f) },	// 左上
+			{ DirectX::XMFLOAT3( 0.5f, -0.5f, 0.0f) },	// 右下
+			{ DirectX::XMFLOAT3( 0.5f,  0.5f, 0.0f) },	// 右上
+		};
+
+		// 2D描画
+		D3D.SquareDraw2D(squares);
+	}
+
+	// バックバッファの内容を画面に表示
+	D3D.mSwapChain->Present(1, 0);
+}
